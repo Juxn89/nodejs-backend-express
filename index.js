b8d64a0b9464aef61092a97b478a6b219e39e27a
Juan Gómez <gomezj01@stepstone.com>
2023-07-14 13:45:25 -0600

    Added other routes

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ app.get('/', (req, res) =>{
   res.send('Server with ExpressJS')
 })
 
+app.get('/categories', (req, res) =>{
+  res.send('Categories page')
+})
+
+app.get('/home', (req, res) =>{
+  res.send('Home page')
+})
+
+app.get('/products', (req, res) =>{
+  res.json({
+    name: 'T-Shirt',
+    color: 'White',
+    price: 1000
+  })
+})
+
 app.listen(SERVER_PORT, () => {
   console.log(`NodeJS Server listening on port: ${SERVER_PORT}`);
 })
