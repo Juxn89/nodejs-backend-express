e2d4a06c586b610381f396779512e6cc8c32ca5a
Juan Gómez <gomezj01@stepstone.com>
2023-07-14 14:09:39 -0600

    Use param on endpoints

--- index.js
+++ index.js
@@ -7,20 +7,51 @@ app.get('/', (req, res) =>{
   res.send('Server with ExpressJS')
 })
 
-app.get('/categories', (req, res) =>{
-  res.send('Categories page')
-})
-
 app.get('/home', (req, res) =>{
   res.send('Home page')
 })
 
+app.get('/categories', (req, res) =>{
+  res.send('Categories page')
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params
+
+  res.json(
+    {
+      categoryId,
+      productId
+    }
+  )
+})
+
 app.get('/products', (req, res) =>{
-  res.json({
-    name: 'T-Shirt',
-    color: 'White',
-    price: 1000
-  })
+  res.json([
+    {
+      name: 'T-Shirt',
+      color: 'White',
+      price: 1000
+    },
+    {
+      name: 'Shorts',
+      color: 'Black',
+      price: 500
+    }
+  ])
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+
+  res.json(
+    {
+      id,
+      name: 'T-Shirt',
+      color: 'White',
+      price: 1000
+    }
+  )
 })
 
 app.listen(SERVER_PORT, () => {
