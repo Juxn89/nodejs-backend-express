d1102dce068055e5c0eaadc4c3550038b6541c1f
Juan Gómez <gomezj01@stepstone.com>
2023-07-14 13:33:10 -0600

    Created Node server with Express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('My App');
+const express = require('express');
+const app = express()
+
+const SERVER_PORT = 3000;
+
+app.get('/', (req, res) =>{
+  res.send('Server with ExpressJS')
+})
+
+app.listen(SERVER_PORT, () => {
+  console.log(`NodeJS Server listening on port: ${SERVER_PORT}`);
+})
