ee3ea3f7fa80dba2c98a514b2a7326a5c1f0ba7a
Juan Gómez <gomezj01@stepstone.com>
2023-07-21 10:50:07 -0600

    Change to MySQL

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  dbHost: process.env.DB_SERVER,
-  dbPort: process.env.DB_PORT,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbDatabase: process.env.DB_NAME
+  dbHost: process.env.MYSQL_DB_SERVER,
+  dbPort: process.env.MYSQL_DB_PORT,
+  dbUser: process.env.MYSQL_DB_USER,
+  dbPassword: process.env.MYSQL_DB_PASSWORD,
+  dbDatabase: process.env.MYSQL_DB_NAME
 }
 
 module.exports = Object.freeze(CONFIG)
